54494edb8a9a972cb9d76a8931db64e0a1d3f158
PN127 <pasch.makhonin20109646@gmail.com>
2024-02-11 19:09:59 +0300

    0.7.1

---
+++ Assets/Cards/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cards
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Player _walker;
+
+        [SerializeField]
+        private Player[] _players;
+        
+
+        private void Start()
+        {            
+            _walker = _players[0];
+        }
+
+        public void MoveTransitiion()
+        {
+            if (_walker == _players[0])
+            {
+                _walker = _players[1];
+            }
+            else
+            {
+                _walker = _players[0];
+            }
+            
+        }
+    }
+}
---
+++ Assets/Cards/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cards
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField]
+        private Deck _deck;
+        public Deck GetDeck => _deck;
+        [SerializeField]
+        private PlayerHand _hand;
+        public PlayerHand GetPlayerHand => _hand;
+
+        private void Start()
+        {
+            _deck = GetComponentInChildren<Deck>();
+            _hand = GetComponentInChildren<PlayerHand>();
+        }
+    }
+}
