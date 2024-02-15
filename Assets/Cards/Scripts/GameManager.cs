eefdb6ddf6a596d62b4a0f417473d063ca94aa8c
PN127 <pasch.makhonin20109646@gmail.com>
2024-02-15 19:10:25 +0300

    0.8.1

--- Assets/Cards/Scripts/GameManager.cs
+++ Assets/Cards/Scripts/GameManager.cs
@@ -8,10 +8,15 @@ namespace Cards
     {
         [SerializeField]
         private Player _walker;
-
+        
+        [SerializeField]
+        private Camera camera;
+        
+        [Space]
         [SerializeField]
         private Player[] _players;
-        
+
+       
 
         private void Start()
         {            
@@ -20,19 +25,39 @@ namespace Cards
 
         public void MoveTransitiion()
         {
+            Vector3 rotation;
+
             if (_walker == _players[0])
             {
                 _walker = _players[1];
+                rotation = new Vector3(90, 180, 0);
             }
             else
             {
                 _walker = _players[0];
-            }            
+                rotation = new Vector3(90, 0, 0);
+            }
+
+            StartCoroutine(CameraRotation(rotation));
         }
 
         public void SelectStartingHand()
         {
             _walker.GetDeck.AddCardsInPlayerHandByStartHand();
+        }
+
+        private IEnumerator CameraRotation(Vector3 FinishRotation)
+        {
+            float t = 0;
+            while (t < 10)
+            {
+                camera.transform.rotation *= Quaternion.Euler(new Vector3(0,0,180) * 0.1f * Time.deltaTime);
+                t += Time.deltaTime;
+                yield return null;
+            }
+
+            camera.transform.rotation = Quaternion.Euler(FinishRotation);
+            Debug.Log("finish");
         }
     }
 }
